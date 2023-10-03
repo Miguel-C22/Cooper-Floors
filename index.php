e985e9a9e56e3a3c3807801d85bde325bc61090e
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2023-10-02 20:36:13 -0500

    Quick Update

--- index.php
+++ index.php
@@ -62,8 +62,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="styles.css?<?php echo filemtime("styles.css") ?>">
+    <link rel="stylesheet" href="form.css?<?php echo filemtime("form.css") ?>">
     <title>Home</title>
 </head>
 <body>
